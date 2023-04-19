e5094a5026e8f33edfe3426bff0f0916a7f26bdf
AbdiSantos <abdisantos1004@gmail.com>
2023-04-19 15:15:17 -0600

    first commit test

---
+++ test-module/__manifest__.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+{
+    'name':'test-module',
+    'sumary':"este es un modulo de test para odoo",
+    'description':"este es solo un modulo de pruebas para odoo",
+    
+    'author':"Abdi Santos",
+    'website':"",
+    'category':"sales",
+    'version':"0.1",
+    
+    'depends':['base'],
+    'data':[
+    
+    ],
+    
+    'demo':[],
+}
